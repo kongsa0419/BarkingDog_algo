a87d4e883a17fd87ce67b8d903b74b2aa0b4ca27
scott <kongsa0419@gmail.com>
2024-06-30 23:28:51 +0900

    개발 환경 (gdb, gitignore)

---
+++ template.cpp
@@ -0,0 +1,20 @@
+/**
+ * name:    
+ * solved:  N
+ */
+
+#include<iostream>
+#include<string>
+#include<algorithm>
+#define PRE_PROC ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); 
+using namespace std;
+
+void run(){
+    /* code HERE */
+    
+}
+int main(void){
+    PRE_PROC
+    run();
+    return 0;
+}

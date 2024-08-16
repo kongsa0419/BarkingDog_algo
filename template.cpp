633cdcd746e0fa0aa7abf8ab5b3d012a04b91de3
scott <kongsa0419@gmail.com>
2024-08-16 18:54:16 +0900

    BFS용 템플릿 업그레이드
    - define X first , Y second
    - STL libraries

--- template.cpp
+++ template.cpp
@@ -8,15 +8,20 @@
 #include<vector>
 #include<algorithm>
 #include<stack>
+#include<queue>
+#include<tuple>
+
+#define X first
+#define Y second
 #define PRE_PROC ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); 
 using namespace std;
 
-void run(){
-    /* code HERE */
-    
-}
+int dy[4] = {0, 1, 0, -1};
+int dx[4] = {1, 0, -1, 0};
+
 int main(void){
     PRE_PROC
-    run();
+    /* code HERE */
+
     return 0;
 }

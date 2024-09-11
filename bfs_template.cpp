3e5f0c220ce30b7346a3673d7076b87878487fa4
scott <kongsa0419@gmail.com>
2024-09-11 16:47:58 +0900

    * code template added *

--- bfs_template.cpp
+++ bfs_template.cpp
@@ -11,11 +11,17 @@
 #include<queue>
 #include<tuple>
 
+#define X first
+#define Y second
 #define PRE_PROC ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); 
 using namespace std;
 
+int dy[4] = {0, 1, 0, -1};
+int dx[4] = {1, 0, -1, 0};
 
 int main(void){
     PRE_PROC
+    /* code HERE */
+
     return 0;
 }
